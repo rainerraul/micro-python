23ae3292b5b83ea1d799fe856b2757c5ed7dc889
Rainer Raul <rainerraul@t-online.de>
2023-08-13 08:39:50 +0200

    Add files via upload

--- webserver/fullwebserver.py
+++ webserver/fullwebserver.py
@@ -5,18 +5,21 @@ import socket
 import network
 import time
 import os
-import re
 
-ssid = "########"
-password = "##########"
+ssid = "kellerbereich"
+password = "54826106"
 
 adc0 = machine.ADC(machine.Pin(26))
+adc1 = machine.ADC(machine.Pin(27))
+adc2 = machine.ADC(machine.Pin(28))
 
 html = """<!DOCTYPE html>
 <html>
 <head> <title>Pico W</title> </head>
-<body> <h1>Pico W</h1>
-<p></p><p><b>%1.3f V</b></p>
+<body> <h3>Analog conversion values</h3>
+<table border='1'><tr><td>ADC0</td><td><b>%1.3f V</td></tr>
+<tr><td>ADC1</td><td><b>%1.3f V</td></tr>
+<tr><td>ADC2</td><td><b>%1.3f V</td></tr></table>
 </body>
 </html>
 """
@@ -33,116 +36,165 @@ posthtml = """
 <tr><td>%s</td><td>%s</td></tr></table>
 </body></html>
 """
+scanhtml = """
+<!DOCTYPE HTML><html><head><title>Scan SSID</title></head><body>
+<h3>Scanresult of Infrastructure networks</h3>
+<table border='1'><th>SSID</th><th>MAC</th><th>SIGNAL</th>%s</table>
+<br><br>
+<table border='1'><tr><td>Server IP</td><td>%s</td></tr>
+<tr><td>Subnetmask</td><td>%s</td></tr>
+<tr><td>Gateway</td><td>%s</td></tr>
+</body></html>
+"""
 
-def connect_to_wlan(on, ssid = None, password = None) :
+
+
+def connect_to_wlan(on=None, ssid=None, password=None):
     global wait
     wait = 0
-        
+    
     wlan = network.WLAN(network.STA_IF)
-    if(on == 1) :
-        wlan.active(on)
+    wlan.active(on)
+    result = wlan.scan()
+        
+    if on == 1:
         wlan.connect(ssid, password)
 
-        while (wlan.status() != 3) :
-                wait += 1
-        
-                if (wait == 10) :
-                    print("can not connect!!")
-                    return False
-                time.sleep(1)
-        
-        return True, wlan.ifconfig()
+        while wlan.status() != 3:
+            wait += 1
 
-    elif(on == 0) :
+            if wait == 10:
+                print("can not connect!!")
+                return False, 0, result
+            time.sleep(1)
+
+    elif on == 0:
         wlan.disconnect()
+        while wlan.status() > 0:
+            wait += 1
+
+            if wait == 10:
+                print("disconnected")
+                return False
+
+            time.sleep(1)
         wlan.active(on)
         return False
-    
-def handle_requests(req, maxpairs) :
-    
+  
+    return True, wlan.ifconfig(), result
+
+
+def handle_requests(req, maxpairs):
+
     key = [" "] * maxpairs
     value = [" "] * maxpairs
     keyvalues = [" "] * maxpairs
-    
+
     getdata = ""
     startGetrequest = 0
     n = 0
-        
+
     requestlen = len(req)
     requestfile = req.split(" ", 3)
-    requestfile[1] = requestfile[1][1:len(requestfile[1])]
-        
-    if(req.find("POST") == 2) :
-        postdata = req.split('\\r\\n\\r\\n')
+    requestfile[1] = requestfile[1][1 : len(requestfile[1])]
+
+    if req.find("POST") == 2:
+        postdata = req.split("\\r\\n\\r\\n")
         keyvalues = postdata[1].split("&")
-                            
-    elif(req.find("GET") == 2) :
+
+    elif req.find("GET") == 2:
         startGetrequest = req.find("?") + 1
         requestfile1 = requestfile[1].split("?")
-                
-        if (startGetrequest > 0) :
+
+        if startGetrequest > 0:
             requestfile[1] = requestfile1[0]
-            getdata = req[startGetrequest:(requestlen - startGetrequest)]
+            getdata = req[startGetrequest : (requestlen - startGetrequest)]
             keyvalues = getdata.split("&")
-        
-        else :
+
+        else:
             return requestfile, key, value
-             
-    for n in range(0, len(keyvalues) - 1) :
+
+    for n in range(0, len(keyvalues) - 1):
         splitdata = keyvalues[n].split("=", 1)
         key[n] = splitdata[0]
         space = splitdata[1].find(" ", 1)
-        value[n] = splitdata[1][0:len(splitdata[1]) - space]
+        value[n] = splitdata[1][0 : len(splitdata[1]) - space]
+
+    return requestfile, key, value
+
+
+def start_server():
+    status = False
+    config = 0
+    scanresult = ""
+    scantable = ""
     
-    return  requestfile, key, value
+    status, config, scanresult = connect_to_wlan(1, ssid, password)
 
-
-def start_server() :
-       
-    if(connect_to_wlan(1, ssid, password)) :
+    if status:
         print("connected!!")
-    
-    try :
+
+    try:
+
         addr = socket.getaddrinfo("0.0.0.0", 80)[0][-1]
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind(addr)
         server.listen(1)
-        
-        while True :
+
+        while True:
             response = ""
             cl, addr = server.accept()
             request = cl.recv(1024)
             request = str(request)
-                   
-            file, k, v = handle_requests(request, 5)
             
-            if(file[1] == "index.html") :
+
+            file, k, v = handle_requests(request, 5)
+
+            if file[1] == "index.html":
                 print(k)
                 print(v)
                 print(file[1])
                 adc0value = ((adc0.read_u16() & 0xFFFF) / 65536) * 3.33
-                response = html % (adc0value)
-        
-            elif(file[1] == "postdata.html") :
+                adc1value = ((adc1.read_u16() & 0xFFFF) / 65536) * 3.33
+                adc2value = ((adc2.read_u16() & 0xFFFF) / 65536) * 3.33
+                
+                response = html % (adc0value, adc1value, adc2value)
+
+            elif file[1] == "postdata.html":
                 print(k)
                 print(v)
                 print(file[1])
                 response = posthtml % (k[0], v[0], k[1], v[1])
-      
+            
+            elif file[1] == "scan.html":
+                print()
+                print(k)
+                print(v)
+                print(file[1])
+                print()
+                
+                status, config, scanresult = connect_to_wlan()
+                
+                for result in scanresult:
+                    scantable = scantable + "<tr>"
+                    scantable = scantable + "<td>" + str(result[0].decode("utf8")) + "</td><td>" + str(result[1].hex(" ")) + "</td><td>" + str(result[3]) + "dB</td>"
+                    scantable = scantable + "</tr>"
+                
+                print("Serveraddress: %s" % (config[0]))
+                print("Netmask: %s" % (config[1]))
+                print("Gateway: %s" % (config[2]))
+                print("Clientaddress: %s" % (addr[0]))
+                               
+                response = scanhtml % (scantable, str(config[0]), str(config[1]), str(config[2]))
+                scantable = ""
+
             cl.send("HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n")
             cl.send(response)
             cl.close()
-    
-    except Exception as e :
+
+    except Exception as e:
         print("exception: ", e)
         time.sleep(5)
         machine.reset()
-        pass
-       
-    start_server()
-  
 
-
-
-
-
+start_server()
