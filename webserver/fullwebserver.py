dbabecee6f20377e87097aacf93f0bd26efd8579
Rainer Raul <rainerraul@t-online.de>
2023-08-15 19:56:18 +0200

    Add files via upload

--- webserver/fullwebserver.py
+++ webserver/fullwebserver.py
@@ -1,13 +1,12 @@
-import io
-import sys
 import machine
 import socket
 import network
 import time
 import os
+import gc
 
-ssid = "########"
-password = "#########"
+ssid = "kellerbereich"
+password = "54826106"
 
 adc0 = machine.ADC(machine.Pin(26))
 adc1 = machine.ADC(machine.Pin(27))
@@ -51,15 +50,14 @@ scanhtml = """
 """
 
 
-
 def connect_to_wlan(on=None, ssid=None, password=None):
     global wait
     wait = 0
-    
+
     wlan = network.WLAN(network.STA_IF)
     wlan.active(on)
     result = wlan.scan()
-        
+
     if on == 1:
         wlan.connect(ssid, password)
 
@@ -83,15 +81,17 @@ def connect_to_wlan(on=None, ssid=None, password=None):
             time.sleep(1)
         wlan.active(on)
         return False
-  
+
     return True, wlan.ifconfig(), result
 
 
 def handle_requests(req, maxpairs):
-
     key = [" "] * maxpairs
     value = [" "] * maxpairs
     keyvalues = [" "] * maxpairs
+    postdata = [""] * 5
+    requestfile = [""] * 3
+    requestfile1 = [""] * 3
 
     getdata = ""
     startGetrequest = 0
@@ -132,33 +132,67 @@ def start_server():
     scanresult = ""
     scantable = ""
     pinvalue = 0
-    pin = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
-   
+    global cl, server
+    
+    pin = [
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        12,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        19,
+        20,
+        21,
+        22,
+        23,
+        24,
+        25,
+        26,
+        27,
+        28,
+        29,
+        30,
+    ]
     v = [""] * 16
     k = [""] * 16
-   
+    config = [""] * 5
+    addr = [""] * 5
+    scanresult = [""] * 32
+
     ch0 = "checked"
     ch1 = ""
-    colorstate= "gray"
-    
+    colorstate = "gray"
+
     status, config, scanresult = connect_to_wlan(1, ssid, password)
 
     if status:
         print("connected!!")
 
     try:
-
         addr = socket.getaddrinfo("0.0.0.0", 80)[0][-1]
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server.bind(addr)
         server.listen(1)
-
+        
         while True:
             response = ""
-            cl, addr = server.accept()
+            cl, addr = server.accept()           
             request = cl.recv(1024)
             request = str(request)
-            
 
             file, k, v = handle_requests(request, 5)
 
@@ -171,43 +205,42 @@ def start_server():
                 adc0value = ((adc0.read_u16() & 0xFFFF) / 65536) * 3.33
                 adc1value = ((adc1.read_u16() & 0xFFFF) / 65536) * 3.33
                 adc2value = ((adc2.read_u16() & 0xFFFF) / 65536) * 3.33
-                
+
                 response = html % (adc0value, adc1value, adc2value)
 
-            elif (file[1] == "mcu.html"):
-
-                if(v[0] == "+"):
-                    v[0] = "0"
-                
+            elif file[1] == "mcu.html":
                 print()
                 print(k)
                 print(v)
                 print(file[1])
                 print()
-                
-                if (v[2] == "write"):
+
+                if v[2] == "write":
+                    if v[0] == "+":
+                        v[0] = "0"
+
                     if v[0] == "LED":
                         pin[29] = machine.Pin(v[0], machine.Pin.OUT)
-                        if (v[1] == "0"):
+                        if v[1] == "0":
                             pin[29].value(int(v[1]))
                             ch0 = "checked"
                             ch1 = ""
                             colorstate = "red"
-                        elif (v[1] == "1"):
+                        elif v[1] == "1":
                             pin[29].value(int(v[1]))
                             ch0 = ""
                             ch1 = "checked"
                             colorstate = "lime"
                         pinvalue = pin[29].value()
-                        
+
                     else:
                         pin[int(v[0])] = machine.Pin(int(v[0]), machine.Pin.OUT)
-                        if (v[1] == "0"):
+                        if v[1] == "0":
                             pin[int(v[0])].value(int(v[1]))
                             ch0 = "checked"
                             ch1 = ""
                             colorstate = "red"
-                        elif (v[1] == "1"):
+                        elif v[1] == "1":
                             pin[int(v[0])].value(int(v[1]))
                             ch0 = ""
                             ch1 = "checked"
@@ -215,29 +248,43 @@ def start_server():
                         pinvalue = pin[int(v[0])].value()
 
                     print(pinvalue)
-                                  
+
                 response = mcuhtml % (v[0], ch0, ch1, colorstate, v[0], pinvalue)
-            
+
             elif file[1] == "scan.html":
                 print()
                 print(k)
                 print(v)
                 print(file[1])
                 print()
-                
+
                 status, config, scanresult = connect_to_wlan()
-                
+
                 for result in scanresult:
                     scantable = scantable + "<tr>"
-                    scantable = scantable + "<td>" + str(result[0].decode("utf8")) + "</td><td>" + str(result[1].hex(" ")) + "</td><td>" + str(result[3]) + "dB</td>"
+                    scantable = (
+                        scantable
+                        + "<td>"
+                        + str(result[0].decode("utf8"))
+                        + "</td><td>"
+                        + str(result[1].hex(" "))
+                        + "</td><td>"
+                        + str(result[3])
+                        + "dB</td>"
+                    )
                     scantable = scantable + "</tr>"
-                
+
                 print("Serveraddress: %s" % (config[0]))
                 print("Netmask: %s" % (config[1]))
                 print("Gateway: %s" % (config[2]))
                 print("Clientaddress: %s" % (addr[0]))
-                               
-                response = scanhtml % (scantable, str(config[0]), str(config[1]), str(config[2]))
+
+                response = scanhtml % (
+                    scantable,
+                    str(config[0]),
+                    str(config[1]),
+                    str(config[2]),
+                )
                 scantable = ""
 
             cl.send("HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n")
@@ -246,7 +293,13 @@ def start_server():
 
     except Exception as e:
         print("exception: ", e)
+      
+        print("Used: ", gc.mem_alloc())
+        print("Free: ", gc.mem_free())
+                
+        cl.close()
+        server.close()
         time.sleep(5)
-        machine.reset()
-
-start_server()
+                             
+while True:        
+    start_server()
